ea32d4f733f5a7e687e5ff982b9cc0aef95335f9
zuberi <51080597+Nassimzuberi@users.noreply.github.com>
2020-01-23 14:50:16 +0100

    rectification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name')}}</title>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="css/app.css">
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    </head>
-    <body>
-        <div id="react-app"></div>
-        <script src="js/app.js"></script>
+@section('content')
 
-    </body>
-
-</html>
+@endsection
