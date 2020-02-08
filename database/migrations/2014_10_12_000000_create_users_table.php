052a5fde857b452e598dd632587d7ae7a7bf8ef4
loudjair <42482922+loudjair@users.noreply.github.com>
2020-02-08 08:34:01 +0100

    Update 2014_10_12_000000_create_users_table.php
    Mise à jour table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            //$table->string('last_name');
-            $table->string('name');
-            //$table->string('first_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday');
+            $table->enum('gender',['Man','Woman']);
             $table->string('email')->unique();
-            //$table->date('birthday');
-            //$table->enum('Sexe',['Man','Woman']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
