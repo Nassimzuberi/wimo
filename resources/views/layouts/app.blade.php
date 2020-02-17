0cefe6f5f0f559da45d9e71b1860e81e3f97aa6b
aniscoquelet <anis.coquelet@gmail.com>
2020-02-17 01:34:15 +0100

    Changements esthétiques, ajustement de la map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Titre -->
     <title>{{ config('app.name', 'Wimo') }} | @yield('title') </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/0a9da0bbd4.js" crossorigin="anonymous"></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -19,45 +21,60 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <!--  <link rel="stylesheet" href="{{ asset('css/styles2.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @yield('extra-script')
-    <!-- LEAFLET -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-  crossorigin=""/>
-  <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-   crossorigin=""></script>
-</head>
-<body>
-    <div id="app">
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{url('/')}}">
-          <img src="{{asset('images/wimo-logo.png')}}" alt="" style="height:43px; filter: drop-shadow(4px 4px 0px #1CE6BE);">
-        </a>
+    <!-- Favicon --> 
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/images/apple-icon-57x57.png')}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/images/apple-icon-60x60.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/images/apple-icon-72x72.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/images/apple-icon-76x76.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/images/apple-icon-114x114.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/images/apple-icon-120x120.png')}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/images/apple-icon-144x144.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/images/apple-icon-152x152.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/apple-icon-180x180.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('/images/android-icon-192x192.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/images/favicon-96x96.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon-16x16.png')}}">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+</head>
+
+
+
+<body>
+
+    <div id="app">
+      <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-white ">
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-
+          
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item dark">
+              <a class="nav-link" href="{{route('map.index')}}"> Map </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('map.index')}}"> Carte </a>
-            </li>
-  
-
           </ul>
+          <a class="navbar-brand mx-auto" href="{{url('/')}}">
+            <img src="{{asset('images/wimo-logo.png')}}" alt="" style="height:43px; filter: drop-shadow(4px 4px 0px #1CE6BE);">
+        </a>
           <ul class="navbar-nav align-items-center">
             <li class="nav-item">
-              <a class="nav-link" href="{{route('cart')}}"> <i class="fas fa-shopping-cart"></i> <span class="badge badge-info">{{Cart::count()}}</span> </A>
+              <a class="nav-link" href="{{route('cart')}}"> <i class="fas fa-shopping-cart"></i> <span class="badge badge-info round-border" style="background-color:#1CE6BE">{{Cart::count()}}</span> </A>
             </li>
           @auth
           <li class="nav-item">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <a id="navbarDropdown" class="nav-link dropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     <img src="{{asset('images/user.png')}}" alt="user-icon" id="user-icon" width='50'> <span class="caret"></span>
                 </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -85,6 +102,7 @@
           @endauth
         </div>
       </nav>
+    </header>
 
     <!--<header>
 
@@ -120,10 +138,12 @@
       </header>
 -->
 
-        <main class="p-4">
+        <main>
             @yield('content')
         </main>
+
     </div>
     @yield('extra-js')
-</body>
-</html>
+  </body>
+  
+  </html>
