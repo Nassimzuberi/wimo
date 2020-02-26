95c54fbf9aa3a5f386b4c08ecbaac545d0a81362
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-26 14:31:14 +0100

    Update controller

---
+++ app/Http/Controllers/InventaireController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use App\Inventaire;
+
+class InventaireController extends Controller
+{
+    
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        $products = Auth::user()->seller->sales;
+        return view('my_inventory',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,$id_announce)
+    {
+        Inventaire::create([
+            "quantity" => $request["quantity"] ?? NULL,
+            "weight" => $request["weight"] ?? NULL,
+            "sale_id" => $id_announce, 
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $inventory = Inventaire::find($id);
+        return view('edit_inventory',compact("inventory"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $inventory = Inventaire::find($id);
+        if($request["type"]=="weight"){
+            $inventory->weight = $request->weight;
+            $inventory->quantity = NULL;
+        }
+        else{
+            $inventory->quantity = $request->quantity;
+            $inventory->weight = NULL;
+        }
+        $inventory->save();
+        return redirect('inventaires')->with('status','inventaire mis à jour');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Inventaire::destroy($id);
+    }
+}
