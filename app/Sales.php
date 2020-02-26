3212555d9d48d8cf05c0a226a938007f9c7000fb
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-26 14:45:44 +0100

    Ajout model Sales (les annonces)

---
+++ app/Sales.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class Sales extends Pivot
+{
+	protected $table = "sales";
+    protected $fillable = [
+   		'price_weight','price_unit','seller_id','product_id',
+   	];
+   	
+   	public function product(){
+   		return $this->belongsTo('App\Product');
+   	}
+
+   	public function inventaire(){
+   		return $this->hasOne("App\Inventaire",'sale_id');
+   	}
+}
