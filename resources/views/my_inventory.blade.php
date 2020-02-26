f4ea2d6b30ff80a0abc59a2c9af27967ff053a00
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-26 15:42:13 +0100

    ajout la vue d'editions de l'inventaire des produits

---
+++ resources/views/my_inventory.blade.php
@@ -0,0 +1,33 @@
+@extends("layouts.app")
+@section("content")
+<h1>Inventaire de mes produits</h1>
+@if(session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+@endif
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>Produit</th>
+			<th>Poids</th>
+			<th>Quantité</th>
+			<th>Modifier</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($products as $product)
+			<tr>
+				<td>{{$product->product->name}}</td>
+				<td>{{$product->inventaire->weight}}</td>
+				<td>{{$product->inventaire->quantity}}</td>
+				<td>
+					<a href="{{route('inventaires.edit',$product->inventaire->id)}}">
+						Modifier
+					</a>
+				</td>
+			</tr>
+		@endforeach
+	</tbody>
+</table>
+@endsection
