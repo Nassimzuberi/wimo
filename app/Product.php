716f36af1a7a89ef39fe2a78b10ab2fde6bd4b5a
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-26 14:43:41 +0100

    Update model product

--- app/Product.php
+++ app/Product.php
@@ -6,15 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable= ['name','img','description','prix_unit','prix_poids','quantity'];
-
-    public function tags(){
-      return $this->belongsToMany('App\Tags','tags_produits');
-    }
-    public function user(){
-        return $this->belongsTo('App\User');
-    }
-    public function commandes(){
-      return $this->hasMany('App\Commande');
-    }
+	public function categories(){
+		return $this->belongsToMany('App\Category','tags');
+	}
 }
