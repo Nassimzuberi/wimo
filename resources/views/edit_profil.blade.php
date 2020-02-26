7ecd6a1320733edef04c2d5caf41354c14e51864
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-26 15:26:47 +0100

    update view edit_profil

--- resources/views/edit_profil.blade.php
+++ resources/views/edit_profil.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 @section('content')
 <h1>Modification de mon profil</h1>
-<form method="post" action="{{route('update_profil')}}">
+<form method="post" action="{{route('comptes.update',compact('compte'))}}">
 	@csrf
+	@method('PUT')
 	<label>Prénom</label>
-	<input type="text" name="first_name" value="{{$user->first_name}}"><br>
+	<input type="text" name="first_name" value="{{$compte->first_name}}"><br>
 	<label>Nom</label>
-	<input type="text" name="last_name" value="{{$user->last_name}}"><br>
+	<input type="text" name="last_name" value="{{$compte->last_name}}"><br>
 	<label>Date de naissance</label>
-    <input type="date" name="birthday" value="{{$user->birthday}}"><br>
+    <input type="date" name="birthday" value="{{$compte->birthday}}"><br>
 	<label>Sexe</label>
-    <input type="radio" name="gender" value="Man" {{$user->gender=="Man"?"checked":""}}>
+    <input type="radio" name="gender" value="Man" {{$compte->gender=="Man"?"checked":""}}>
     <span>Homme</span>
-    <input type="radio" name="gender" value="Woman" {{$user->gender=="Woman"?"checked":""}}>
+    <input type="radio" name="gender" value="Woman" {{$compte->gender=="Woman"?"checked":""}}>
     <span>Femme</span><br>    
 	<input type="submit" value="Valider">
 </form>
