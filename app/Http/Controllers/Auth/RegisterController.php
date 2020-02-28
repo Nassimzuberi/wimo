90d98ee293dd80401710051d0a1ec08028a8797c
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-02-28 01:22:37 +0100

    update RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::SELLER_REGISTER_FORM;
 
     /**
      * Create a new controller instance.
