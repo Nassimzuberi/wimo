6281c48b34f05c2f450e4c18a0700ca4abd66bc6
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2020-04-08 17:11:58 +0200

    Correctif

--- public/js/app.js
+++ public/js/app.js
@@ -69191,8 +69191,8 @@ var getTopSales = function getTopSales() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\wimo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\wimo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\nassi\wimo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\nassi\wimo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
