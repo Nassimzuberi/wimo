0e491dffde203e1df354ca70f23c972483b9fa05
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2020-04-16 10:30:23 +0200

    Rectification image sales admin

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TopSales from "./TopSales";
-import SendTicket from "./SendTicket";
 
 
 if (document.getElementById('top')) {
     ReactDOM.render(<TopSales />, document.getElementById('top'));
 }
 
-
-if (document.getElementById('sendticket')) {
-    ReactDOM.render(<SendTicket />, document.getElementById('sendticket'));
-}
