3ae14d3cc194f54905b2a758468b6f527cd267fe
Loudjair Mauvais <mr.loudjair@gmail.com>
2020-05-11 03:17:38 +0200

    donnée sql postgres

--- database/postgres/pays.sql
+++ database/postgres/pays.sql
@@ -1,20 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 07, 2013 at 08:28 PM
--- Server version: 5.5.28
--- PHP Version: 5.3.10-1ubuntu3.4
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 -- --------------------------------------------------------
 
@@ -30,10 +14,10 @@ CREATE TABLE IF NOT EXISTS pays (
   nom_en_gb varchar(45) NOT NULL,
   nom_fr_fr varchar(45) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY alpha2 (alpha2),
-  UNIQUE KEY alpha3 (alpha3),
-  UNIQUE KEY code_unique (code)
-) DEFAULT CHARSET=utf8;
+  UNIQUE(alpha2),
+  UNIQUE(alpha3),
+  UNIQUE(code)
+);
 
 --
 -- Dumping data for table `pays`
